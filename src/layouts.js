984efeba3b7bf0c2ae95a1d1e31aa36e6d660237
Valerii Voronkov <scorpirat@gmail.com>
2021-03-18 18:10:21 +0300

    Add russian language to layouts.

--- src/layouts.js
+++ src/layouts.js
@@ -21,6 +21,13 @@ module.exports = {
 			"{shiftl} z x c v b n m , . / {shiftr}",
 			"{next} {space} {accept}"
 		],
+		default_ru: [
+			"` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
+			"{tab} й ц у к е н г ш щ з х ъ \\",
+			"{caps} ф ы в а п р о л д ж э {enter}",
+			"{shiftl} я ч с м и т ь б ю . {shiftr}",
+			"{next} {space} {accept}"
+		],
 		shifted: [
 			"~ ! @ # $ % ^ & * ( ) _ + {backspace}",
 			"{tab} Q W E R T Y U I O P { } |",
@@ -28,18 +35,24 @@ module.exports = {
 			"{shiftl} Z X C V B N M < > ? {shiftr}",
 			"{next} {space} {accept}"
 		],
+		shifted_ru: [
+			"Ё ! \" № ; % : ? * ( ) _ + {backspace}",
+			"{tab} Й Ц У К Е Н Г Ш Щ З Х Ъ |",
+			"{caps} Ф Ы В А П Р О Л Д Ж Э {enter}",
+			"{shiftl} Я Ч С М И Т Ь Б Ю , {shiftr}",
+			"{next} {space} {accept}"
+		],
 		
 		capsed: [
-			"` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
-			"{tab} Q W E R T Y U I O P [ ] \\",
-			"{caps} A S D F G H J K L ; ' {enter}",
-			"{shiftl} Z X C V B N M , . / {shiftr}", 
+			"Ё 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
+			"{tab} Й Ц У К Е Н Г Ш Щ З Х Ъ \\",
+			"{caps} Ф Ы В А П Р О Л Д Ж Э {enter}",
+			"{shiftl} Я Ч С М И Т Ь Б Ю . {shiftr}", 
 			"{next} {space} {accept}"
 		]		
 	},
 
 	"compact": {
-
 		_meta: {
 			"default": { keySet: "default", text: "abc", classes: "control"},
 			"alpha": { keySet: "default", text: "Abc", classes: "control"},
@@ -53,16 +66,16 @@ module.exports = {
 		},
 
 		default: [
-			"q w e r t y u i o p",
-			" a s d f g h j k l ",
-			"{shift} z x c v b n m {backspace}",
+			"й ц у к е н г ш щ з",
+			" ф ы в а п р о л д ",
+			"{shift} я ч с м и т ь {backspace}",
 			"{numbers} , {space} . {next} {accept}"
 		],
 
 		shifted: [
-			"Q W E R T Y U I O P",
-			" A S D F G H J K L ",
-			"{default} Z X C V B N M ",
+			"Й Ц У К Е Y U I O P",
+			" Ф Ы В А П Р О Л Д ",
+			"{default} Я Ч С М И Т Ь ",
 			"{numbers} _ {space} {backspace} {next} {accept}"
 		],
 
